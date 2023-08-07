f4878c7b5d3464684053a641ebf3bb63ce276aa1
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-07 22:46:50 +0300

    update check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,33 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: pointer to the head of the list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-    listint_t *tortoise, *hare;
+listint_t *tortoise, *hare;
 
-    if (!list)
-        return (0);
+if (!list)
+{
+return (0);
+}
 
-    tortoise = list;
-    hare = list;
+tortoise = list;
+hare = list;
 
-    while (hare != NULL && hare->next != NULL)
-    {
-        tortoise = tortoise->next;          // Tortoise moves one step
-        hare = hare->next->next;            // Hare moves two steps
+while (hare != NULL && hare->next != NULL)
+{
+tortoise = tortoise->next;
+hare = hare->next->next;
 
-        if (tortoise == hare)               // If they meet, then there's a cycle
-            return (1);
-    }
+if (tortoise == hare)
+{
+return (1);
+}
+}
 
-    return (0);                             // If we've gone through the whole list without the pointers meeting, no cycle
+return (0);
 }
