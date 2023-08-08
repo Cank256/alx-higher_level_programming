92c8a751af4339336dd7b7c6018d1fc47888d16e
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-08 21:41:54 +0300

    update print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
-    print("{}".format(chr(122 - i) if i % 2 == 0 else chr(90 - (i - 1))), end="")
+    c = chr(122 - i) if i % 2 == 0 else chr(90 - (i - 1))
+    print("{}".format(c), end="")
