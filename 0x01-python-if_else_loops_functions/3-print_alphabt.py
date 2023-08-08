b2802bc93f1d3002acf9db66af7ab459f0a2a6d5
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-08 12:20:36 +0300

    update print alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for i in range(26):
     if i == 4 or i == 16:  # Skip 'e' (ASCII value 101) and 'q' (ASCII value 113)
         continue
-    print(chr(97 + i), end='')
+    print("{}".format(chr(97 + i)), end='')
