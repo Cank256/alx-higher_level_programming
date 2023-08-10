7bbbf5e9c06b7f918e4e845c3e528837820df471
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-10 23:01:32 +0300

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    argv = sys.argv[1:]  # Excluding the script name itself
+    num_args = len(argv)
+
+    if num_args == 1:
+        print(f"{num_args} argument:")
+    else:
+        print(f"{num_args} arguments." if num_args == 0 else f"{num_args} arguments")
+
+    for index, arg in enumerate(argv, start=1):
+        print(f"{index}: {arg}")
+
+if __name__ == "__main__":
+    main()
