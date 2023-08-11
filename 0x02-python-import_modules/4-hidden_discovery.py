a10abbc5ef3126d6d556d40cd3acf7a1c66c3d7c
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-11 07:58:33 +0300

    hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+def main():
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    for name in sorted(names):
+        print(name)
+
+if __name__ == "__main__":
+    main()
