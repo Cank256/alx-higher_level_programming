48e5496a68605d2170e8b46a59d9dbd8e9b3ae78
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-12 19:50:13 +0300

    update add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+def main():
+    a = 1
+    b = 2
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+
+if __name__ == '__main__':
+    main()
