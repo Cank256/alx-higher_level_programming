b704a69bc75782f4831cc17418177db84b174200
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-13 15:01:47 +0300

    betty

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,34 +1,45 @@
 #include "lists.h"
 
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Double pointer to the head node of the linked list.
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ * An empty list is considered a palindrome.
+ */
 int is_palindrome(listint_t **head)
 {
-    if (head == NULL || *head == NULL)
-        return (1); // An empty list is considered a palindrome
+if (head == NULL || *head == NULL)
+{
+return (1);
+}
 
-    listint_t *slow = *head, *fast = *head, *prev = NULL;
+listint_t *slow = *head, *fast = *head, *prev = NULL;
 
-    // Find the middle of the list
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-        listint_t *next = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = next;
-    }
+while (fast != NULL && fast->next != NULL)
+{
+fast = fast->next->next;
+listint_t *next = slow->next;
+slow->next = prev;
+prev = slow;
+slow = next;
+}
 
-    if (fast != NULL) // Odd number of elements, skip the middle element
-        slow = slow->next;
+if (fast != NULL)
+{
+slow = slow->next;
+}
 
-    // Compare the reversed first half with the second half
-    while (prev != NULL && slow != NULL)
-    {
-        if (prev->n != slow->n)
-            return (0); // Not a palindrome
+while (prev != NULL && slow != NULL)
+{
+if (prev->n != slow->n)
+{
+return (0);
+}
 
-        prev = prev->next;
-        slow = slow->next;
-    }
+prev = prev->next;
+slow = slow->next;
+}
 
-    return (1); // It is a palindrome
+return (1);
 }
