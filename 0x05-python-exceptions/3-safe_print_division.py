9a9b6638b7087ad1aad52b9c5845e28cb1220ac2
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-28 14:55:58 +0300

    Exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        try:
+            print("Inside result: {}".format(result))
+        except NameError:
+            print("Inside result: None")
+
+        return result
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    try:
+        for i in range(list_length):
+            try:
+                element1 = my_list_1[i]
+                element2 = my_list_2[i]
+                if not isinstance(element1, (int, float)):
+                    if not isinstance(element2, (int, float)):
+                        print("wrong type")
+                        result_list.append(0)
+                elif element2 == 0:
+                    print("division by 0")
+                    result_list.append(0)
+                else:
+                    result = element1 / element2
+                    result_list.append(result)
+            except IndexError:
+                print("out of range")
+                result_list.append(0)
+    finally:
+        return result_list
