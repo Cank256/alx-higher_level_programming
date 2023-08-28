4d3cf45956e48e8659c37591535dd32efaf6fa8b
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-28 15:48:14 +0300

    advanced

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
+
+
+def divide(a, b):
+    return a / b
+
+
+def square(x):
+    return x * x
