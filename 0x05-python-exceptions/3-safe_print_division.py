0ac47dd71de4be2f4f7b0e8173a9ee430bf7e3a1
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-28 15:28:19 +0300

    2 and 3 updated

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None  # Initialize result with a default value
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
+        pass  # You can leave it as None
     finally:
         try:
             print("Inside result: {}".format(result))
         except NameError:
             print("Inside result: None")
-
+    
+    if result is not None:
         return result
+    else:
+        return None
