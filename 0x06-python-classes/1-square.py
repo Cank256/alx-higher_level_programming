6e7ee64f1f7bbaee4020bcadcd45e52cc63a82c6
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-31 09:56:19 +0300

    document

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (float): The size of each side of the square.
+    """
+    
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (float): The size of each side of the square.
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of each side of the square.
+    """
+    
     def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int, optional): The size of each side of the square. Defaults to 0.
+        
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than zero.
+        """
         if not isinstance(size, int):
             raise TypeError('Size must be an integer')
         if size < 0:
-            raise ValueError('Size must be greater than zero')
+            raise ValueError('Size must be greater than or equal to zero')
         self.__size = size
