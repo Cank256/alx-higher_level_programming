ef6a9227580002a786c42964db8939b6c5c0852f
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-31 09:59:51 +0300

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,17 +3,9 @@ class Square:
     This class represents a square.
 
     Attributes:
-        side_length (float): The length of each side of the square.
+        None
 
     Methods:
         None
     """
-
-    def __init__(self, side_length):
-        """
-        Initializes a Square instance.
-
-        Args:
-            side_length (float): The length of each side of the square.
-        """
-        self.side_length = side_length
+    pass
