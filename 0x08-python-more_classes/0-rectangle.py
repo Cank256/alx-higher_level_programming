daf9a181425c38cd79a339e9942d98f6ce47822b
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-04 12:34:48 +0300

    empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
+An empty python class to represent a rectangle
+"""
+
+
 class Rectangle:
+    """
+    This class represents a rectangle.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
     pass
