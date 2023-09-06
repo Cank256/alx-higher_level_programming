9fbfaa9593538edbcc0f7af34efb2ba317f5fe38
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-06 14:14:50 +0300

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,11 +5,3 @@ class LockedClass:
             self.__dict__[name] = value
         else:
             raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
-
-# Example usage:
-obj = LockedClass()
-obj.first_name = "John"  # This is allowed
-print(obj.first_name)    # Output: John
-
-# Trying to set another attribute will raise an AttributeError
-obj.last_name = "Doe"    # This will raise an AttributeError
