976d28cc45053ea39364c2a5978bef61a3de084f
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-06 14:17:45 +0300

    locked class

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
+    """ Get a magic string by interating a number of times"""
     return "BestSchool"
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,4 +4,6 @@ class LockedClass:
         if name == "first_name":
             self.__dict__[name] = value
         else:
-            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'"
+            )
