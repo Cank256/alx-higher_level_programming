36d5dbe32eff835b91aec1189b8482bc098e98ed
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-30 22:45:50 +0300

    update test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,57 +1,33 @@
 #!/usr/bin/python3
 import unittest
+from max_integer import max_integer
 """
 A max integer test
 """
 
-
-def max_integer(lst=[]):
-    # Your implementation of the max_integer function goes here
-    pass
-
-
 class TestMaxInteger(unittest.TestCase):
 
-    def test_max_integer_positive_values(self):
-        # Test when the list contains positive integers
-        result = max_integer([1, 3, 5, 7, 9])
-        self.assertEqual(result, 9)
+    def test_max_integer(self):
+        # Test with a list of positive integers
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
-    def test_max_integer_negative_values(self):
-        # Test when the list contains negative integers
-        result = max_integer([-1, -3, -5, -7, -9])
-        self.assertEqual(result, -1)
+        # Test with a list of negative integers
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
 
-    def test_max_integer_mixed_values(self):
-        # Test when the list contains mixed positive and negative integers
-        result = max_integer([-2, 4, -6, 8, -10])
-        self.assertEqual(result, 8)
+        # Test with a list containing both positive and negative integers
+        self.assertEqual(max_integer([-1, 2, -3, 4, -5]), 4)
 
-    def test_max_integer_single_value(self):
-        # Test when the list contains a single value
-        result = max_integer([42])
-        self.assertEqual(result, 42)
+        # Test with a list containing duplicate values
+        self.assertEqual(max_integer([1, 2, 2, 1, 5]), 5)
 
-    def test_max_integer_empty_list(self):
-        # Test when the list is empty, should return None
-        result = max_integer([])
-        self.assertIsNone(result)
+        # Test with an empty list, should return None
+        self.assertIsNone(max_integer([]))
 
-    def test_max_integer_float_values(self):
-        # Test when the list contains floating-point numbers
-        result = max_integer([1.5, 2.7, 3.9])
-        self.assertEqual(result, 3.9)
+        # Test with a list of one element, should return that element
+        self.assertEqual(max_integer([42]), 42)
 
-    def test_max_integer_mixed_numbers(self):
-        # Test when the list contains mixed integers and floating-point numbers
-        result = max_integer([1, 2.5, 3, 4.7, 5])
-        self.assertEqual(result, 5)
-
-    def test_max_integer_with_duplicates(self):
-        # Test when the list contains duplicate values
-        result = max_integer([4, 7, 4, 9, 7, 7])
-        self.assertEqual(result, 9)
-
+        # Test with a large list
+        self.assertEqual(max_integer(list(range(1, 10001))), 10000)
 
 if __name__ == '__main__':
     unittest.main()
