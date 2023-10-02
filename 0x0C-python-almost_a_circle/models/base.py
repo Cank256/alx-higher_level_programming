3bc8cdd3ae5f044797ca1cc258f315b28d250c98
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-02 16:35:01 +0300

    start again

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,56 +1,16 @@
 #!/usr/bin/python3
-"""
-    Base class for managing unique identifiers for objects.
-
-    This class is designed to be the base for other classes
-    in the project to manage the 'id' attribute. It ensures that each
-    object created from derived classes has a unique 'id' by either
-    accepting a provided 'id' or generating one based on an internal counter.
-
-    Attributes:
-        __nb_objects (int): A private class attribute to keep track
-        of the number of objects created.
-
-    Methods:
-        __init__(self, id=None):
-            Constructor for the Base class.
-
-            Args:
-                id (int, optional): An integer identifier. If provided, it sets
-                                    the 'id' attribute to this value. If not
-                                    provided, it increments the internal
-                                    counter and assigns the new value as the
-                                    'id' attribute.
-
-    Examples:
-        >>> obj1 = Base()
-        >>> obj1.id
-        1
-
-        >>> obj2 = Base()
-        >>> obj2.id
-        2
-
-        >>> obj3 = Base(100)
-        >>> obj3.id
-        100
-"""
-
+"""Base module."""
 
 class Base:
+    """Base class for managing id attribute."""
 
-    __nb_objects = 0
+    __nb_objects = 0  # private class attribute
 
     def __init__(self, id=None):
-        """
-        Initialize a Base object.
+        """Initialize the Base class.
 
         Args:
-            id (int, optional): An integer identifier. If provided,
-                                it sets the 'id' attribute to this value.
-                                If not provided, it increments the internal
-                                counter and assigns the new value as the
-                                'id' attribute.
+            id (int): The id for the Base instance. If not provided, it will be auto-generated.
         """
         if id is not None:
             self.id = id
