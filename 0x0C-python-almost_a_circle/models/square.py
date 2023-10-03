289ac9ef731b7e7f17e1d80c06987a26e321dbfe
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-03 07:14:04 +0300

    square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,12 +28,45 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """Setter for the size attribute."""
+        """Setter for the size attribute,
+        assigns to width and height.
+
+        Args:
+            value (int): The new size value.
+        """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """Return a string representation of the Square instance."""
+        """Return a string representation of the
+        Square instance."""
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width
-            )
+            self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Assign attributes using both *args and **kwargs.
+
+        Args:
+            *args: List of arguments in the
+            order (id, size, x, y).
+            **kwargs: Keyword arguments representing
+            attribute assignments.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
