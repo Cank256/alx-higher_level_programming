8374ded2d823ea24623d37b22c9cedd150c27abc
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 12:06:08 +0300

    list tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,8 @@
 -- A script that lists all the tables of a database in your MySQL server.
-USE $1;
+
+-- Set the user-defined variable @dbname to the first argument passed to the script
+SET @dbname = ?;
+
+-- Use the selected database to show tables
+USE @dbname;
 SHOW TABLES;
