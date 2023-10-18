fed06ac3771675f12a7eff4999e0d94defe2237a
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 21:30:37 +0300

    create user update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create or update user_0d_1 with all privileges and set the password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *. * TO 'user_0d_1'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
