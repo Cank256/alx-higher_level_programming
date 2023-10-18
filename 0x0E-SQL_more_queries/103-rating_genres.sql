cfc17a656ebe4c81f473a81700c531eb358aaf80
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 22:28:50 +0300

    rating genres update again

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- List genres by their rating sum
-SELECT tv_genres.name, SUM(tv_shows_ratings.rate) AS rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_shows ON tv_genres.id = tv_shows.genre_id
-INNER JOIN tv_shows_ratings ON tv_shows.id = tv_shows_ratings.show_id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
