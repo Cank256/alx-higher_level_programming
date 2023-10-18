60158960f9a8a146cbc3b5f797fe13a82b3306b8
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 16:23:52 +0300

    full table update

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,12 +1,11 @@
 -- script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server
 
-SELECT 
-    TABLE_NAME AS 'Table',
-    CONCAT('CREATE TABLE `', TABLE_NAME, '` (\n', GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE, ' ', IF(IS_NULLABLE = 'NO', 'NOT NULL', 'DEFAULT NULL'), ',\n'), ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME, ' COLLATE=', COLLATION_NAME) AS 'Create Table' 
-FROM information_schema.tables t
-JOIN information_schema.columns c
-ON t.table_name = c.table_name
-WHERE t.table_schema = 'hbtn_0c_0'
-AND t.table_name = 'first_table'
-GROUP BY c.table_name;
+SELECT
+    'first_table' AS 'Table',
+    CONCAT('CREATE TABLE `first_table` (\n', GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE, ' ', IF(IS_NULLABLE = 'NO', 'NOT NULL', 'DEFAULT NULL'), ',\n'), ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME, ' COLLATE=', COLLATION_NAME) AS 'Create Table'
+FROM
+    information_schema.columns
+WHERE
+    table_schema = 'hbtn_0c_0'
+    AND table_name = 'first_table';
