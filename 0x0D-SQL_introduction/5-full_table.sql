c2abdcc74cc8a44063597754ad4c6b96263e7a08
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 16:12:56 +0300

    full table update

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,8 +1,5 @@
 -- script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server
-
--- Prepare and execute a dynamic SQL statement to retrieve table information
-SET @query = CONCAT('SELECT COLUMN_NAME, DATA_TYPE, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = "', ? , '" AND TABLE_NAME = "first_name"');
-PREPARE stmt FROM @query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+SELECT column_name, data_type, column_default, is_nullable, character_maximum_length, column_type, column_key, extra
+FROM information_schema.columns
+WHERE table_name = 'first_table'
