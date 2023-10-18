cff7e59c9cda04d990fcdd42acece5b7ce5744c2
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 11:22:36 +0300

    we go

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- MySQL script to insert a new row in the table first_table (database hbtn_0c_0) 
 -- with id = 89 and name = Best School, passing the database name as an argument of the mysql command
-USE hbtn_0c_0;
+USE @dbname;
 INSERT INTO first_table (id, name) VALUES (89, 'Best School');
