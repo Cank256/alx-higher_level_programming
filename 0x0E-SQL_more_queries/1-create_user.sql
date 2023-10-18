971c0cef5bafae0cb12880ba2ccc55eb911400ff
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 20:15:37 +0300

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create or update user_0d_1 with all privileges and set the password
+CREATE OR REPLACE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
