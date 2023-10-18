b870d9793c76f6a6f1270e47aa61f4e00792beb3
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 19:55:37 +0300

    temperatures update temp colum

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Select the top 3 cities with the highest temperatures during July and August
-SELECT city, MAX(value) as avg_temp
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 WHERE month IN ('July', 'August')
 GROUP BY city
