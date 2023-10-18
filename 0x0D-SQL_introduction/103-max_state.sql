4314bddd9155a0ede7a0f4e7c79e01e6ec564dd4
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 19:51:41 +0300

    temperatures update temp colum

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Select the maximum temperature for each state, ordered by state name
-SELECT state, MAX(temperature) as max_temp
+SELECT state, MAX(value) as max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
