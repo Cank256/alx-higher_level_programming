e13270d1c1dab79bbff754b0b947b9df01ae5572
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 16:04:42 +0300

    list update

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,8 +1,2 @@
--- Set the database name using a user-defined variable
-SET @dbname = ?;
-
--- Use the selected database to show tables
-USE @dbname;
-
 -- List all tables in the selected database
 SHOW TABLES;
