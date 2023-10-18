b29a7b192e5467f6b2e75725e8b32a91d29b7abf
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 19:45:42 +0300

    uavg temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Calculate the average temperature in Fahrenheit by city and order by temperature (descending)
-SELECT city, AVG(temperature) as avg_temperature_fahrenheit
+SELECT city, AVG(value) as avg_temperature_fahrenheit
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temperature_fahrenheit DESC;
