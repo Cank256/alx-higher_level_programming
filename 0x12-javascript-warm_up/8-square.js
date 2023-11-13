67bfe474da81607048ec3d365ee6c8306d85ea33
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-13 19:53:37 +0300

    Javscriptor 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,11 @@
 const size = process.argv[2];
 
 if (isNaN(parseInt(size))) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   const squareSize = parseInt(size);
 
   for (let i = 0; i < squareSize; i++) {
-    console.log("X".repeat(squareSize));
+    console.log('X'.repeat(squareSize));
   }
 }
-
