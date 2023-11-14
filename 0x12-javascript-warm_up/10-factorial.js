4d6a35f023db486ef9853ea15e098ba558121798
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-14 10:21:48 +0300

    Javscriptor 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
 
+const computeFactorial = (n) => {
+  if (isNaN(n)) {
+    console.log(1);
+  } else {
+    const factorial = (n) => (n <= 1 ? 1 : n * factorial(n - 1));
+    console.log(factorial(n));
+  }
+};
 
+const myNum = parseInt(process.argv[2]);
+computeFactorial(myNum);
