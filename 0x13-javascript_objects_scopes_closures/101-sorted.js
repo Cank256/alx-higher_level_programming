bf276fee520668904d4eeb0d6f9725236bf84348
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-15 11:02:08 +0300

    prettie checker

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,9 +2,9 @@
 const dict = require('./101-data').dict;
 
 const usersByOccurrence = Object.entries(dict).reduce((result, [user, occurrence]) => {
-    result[occurrence] = result[occurrence] || [];
-    result[occurrence].push(user);
-    return result;
+  result[occurrence] = result[occurrence] || [];
+  result[occurrence].push(user);
+  return result;
 }, {});
 
 console.log(usersByOccurrence);
