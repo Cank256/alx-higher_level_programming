5778cebab6eddabcbe9030c15d81433ef4962bc6
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-22 08:48:54 +0300

    we goo

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+def list_states(username, password, database):
+    """List all states from the database hbtn_0e_0_usa"""
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve all states, sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and the connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Check if all three command-line arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract command-line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Call the function to list states
+    list_states(username, password, database)
