93d322bc40614476ce167f3e576bad17d756e027
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-12-02 09:25:18 +0300

    networking update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the provided URL and displays the body of the response for a 200 status code.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_body}" "$1"
