cc33f6ba6dea964d0029e9e2079ca7dd42aec6f8
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-01-10 13:00:38 +0300

    Add other scripts with code chcker satisfied

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,3 +1,5 @@
 // A Jquery script that updates the text color of the <header> element to red (#FF0000)
 
-$('header').css('color', '#FF0000')
+$(document).ready(function () {
+  $('header').css('color', '#FF0000');
+});
